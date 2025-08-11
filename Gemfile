7bbcf9a37fc0340cfbf7af31e120d939f1f7de1e
Sahil Sapehia <officialsahilsapehia03@gmail.com>
2025-08-11 14:00:23 +0530

    add restrictions based on login/non-login users

--- Gemfile
+++ Gemfile
@@ -65,3 +65,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem "msgpack", "~> 1.8"
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,35 +1,35 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
-  def edit
-      @article = Article.find(params[:id])
-  end
-
-  def new 
+  def new
     @article = Article.new
   end
 
+  def edit
+  end
+
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.user = User.first 
+    @article = Article.new(article_params)
+    @article.user = current_user
     if @article.save
-      flash[:notice] = "Article was successfully created."
+      flash[:notice] = "Article was created successfully."
       redirect_to @article
-    else 
+    else
       render 'new'
     end
   end
-  
+
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else
@@ -38,9 +38,25 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    if current_user != @article.user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article
+    end
   end
 
 end
